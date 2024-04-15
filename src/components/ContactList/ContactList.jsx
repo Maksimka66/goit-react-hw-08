8c5cabcf89bea5e5e40fa788f83545c37fa3cbec
Maksimka66 <maxfese@gmail.com>
2024-04-15 17:35:39 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,22 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
 import Contact from "../Contact/Contact";
 import { selectFilteredContacts } from "../../redux/filters/selectors";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 
 import styles from "./ContactList.module.css";
+import { fetchContacts } from "../../redux/contacts/operations";
 
 const ContactList = () => {
   const filteredList = useSelector(selectFilteredContacts);
+  const isloggedIn = useSelector(selectIsLoggedIn);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isloggedIn) {
+      dispatch(fetchContacts());
+    }
+  }, [dispatch, isloggedIn]);
 
   return (
     <ul className={styles.listContacts}>
